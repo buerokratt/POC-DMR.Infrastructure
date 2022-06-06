b584c9680e0c000549b05868c37712f3fa5b5e48
Adele-Churchill-13 <ACHURCHILL@microsoft.com>
2022-06-06 11:23:44 +0100

    Add relevant resource templates

--- infrastructure/modules/resource_templates/storage_account/main.tf
+++ infrastructure/modules/resource_templates/storage_account/main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_storage_account" "storage_account" {
-  name = var.name
-  resource_group_name = var.resource_group_name
-  location = var.location
-  account_tier = var.account_tier
-  account_replication_type = var.account_replication_type
+  name                     = var.name
+  resource_group_name      = var.resource_group_name
+  location                 = "uksouth"
+  account_replication_type = "ZRS"
+  account_tier             = "Standard"
 }
--- infrastructure/modules/resource_templates/storage_container/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "id" {
-  description = "Specifies the Id of the storage container created."
-  value       = azurerm_storage_container.storage_container.id
-}
-
-output "name" {
-  description = "Specifies the name of the storage container created."
-  value       = azurerm_storage_container.storage_container.name
-}
