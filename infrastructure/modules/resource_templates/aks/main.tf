9ed43ca46133f82daa3a55665d0dfb43e182203f
Ahmed Zaman <ahmedrzaman@gmail.com>
2022-06-08 14:50:45 +0100

    Encode kubeconfig before it is stored in GH

--- infrastructure/modules/resource_templates/aks/main.tf
+++ infrastructure/modules/resource_templates/aks/main.tf
@@ -24,44 +24,16 @@ resource "azurerm_kubernetes_cluster" "aks" {
   }
 }
 
-data "github_repository" "repo" {
-  full_name = "buerokratt/Infrastructure"
+data "github_actions_public_key" "infra" {
+  repository = "../buerokratt/Infrastructure"
 }
-
-# resource "github_actions_organization_secret" "store_kubeconfig" {
-#   secret_name             = "Kubeconfig__${replace(var.aks_name, "-", "_")}"
-#   visibility              = "selected"
-#   encrypted_value         = base64encode(azurerm_kubernetes_cluster.aks.kube_config_raw)
-#   selected_repository_ids = [data.github_repository.repo.repo_id]
-# }
-
-# data "github_actions_public_key" "infra" {
-#   repository = "../buerokratt/Infrastructure"
-# }
 
 resource "github_actions_secret" "store_kubeconfig" {
   repository      = "../buerokratt/Infrastructure"
   secret_name     = "Kubeconfig__${replace(var.aks_name, "-", "_")}"
-  encrypted_value = azurerm_kubernetes_cluster.aks.kube_config_raw
+  encrypted_value = base64encode(azurerm_kubernetes_cluster.aks.kube_config_raw)
   depends_on = [
-    # data.github_actions_public_key.infra,
+    data.github_actions_public_key.infra,
     azurerm_kubernetes_cluster.aks
   ]
 }
-
-# resource "null_resource" "store_kubeconfig" {
-#   triggers = {
-#     always_run = "${local.current_timestamp}"
-#   }
-
-#   provisioner "local-exec" {
-#     command     = <<CMD
-#     gh secret set kubeconfig-${var.aks_name} < "${azurerm_kubernetes_cluster.aks.kube_config_raw}"
-# CMD
-#     interpreter = ["bash", "-c"]
-#   }
-
-#   depends_on = [
-#     azurerm_kubernetes_cluster.aks 
-#   ]
-# }
