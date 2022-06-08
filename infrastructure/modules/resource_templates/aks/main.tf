081a9c9f1b2b13a7718f8f0247ed1fa756478531
Ahmed Zaman <ahmedrzaman@gmail.com>
2022-06-09 00:59:11 +0100

    Set dns_prefix and node_resource_group

--- infrastructure/modules/resource_templates/aks/main.tf
+++ infrastructure/modules/resource_templates/aks/main.tf
@@ -6,7 +6,8 @@ resource "azurerm_kubernetes_cluster" "aks" {
   name                = var.aks_name
   location            = data.azurerm_resource_group.resource_group.location
   resource_group_name = data.azurerm_resource_group.resource_group.name
-  dns_prefix          = var.aks_name
+  dns_prefix          = "${var.aks_name}-ingress"
+  node_resource_group = "${var.aks_name}-nodes-rg"
 
   default_node_pool {
     name       = "default"
