f1fc443d901d8928dbfdcbba4af7aacce989a942
Ahmed Zaman <ahmedrzaman@gmail.com>
2022-06-10 21:02:23 +0100

    Create the nodes resource group explicitly

--- infrastructure/modules/resource_templates/aks/main.tf
+++ infrastructure/modules/resource_templates/aks/main.tf
@@ -1,20 +1,19 @@
-locals {
-  public_ip_name       = "${var.name}-pip"
-  public_ip_domain     = "${var.name}-ingress"
-  nodes_resource_group = "${var.name}-nodes-rg"
-}
-
 data "azurerm_resource_group" "resource_group" {
   name = var.resource_group_name
 }
 
+resource "azurerm_resource_group" "nodes_resource_group" {
+  name     = "${var.name}-nodes-rg"
+  location = data.azurerm_resource_group.resource_group.location
+}
+
 resource "azurerm_public_ip" "aks_pip" {
-  name                = local.public_ip_name
-  resource_group_name = local.nodes_resource_group
-  location            = data.azurerm_resource_group.resource_group.location
+  name                = "${var.name}-pip"
+  resource_group_name = azurerm_resource_group.nodes_resource_group.name
+  location            = azurerm_resource_group.nodes_resource_group.location
   allocation_method   = "Static"
   sku                 = "Standard"
-  domain_name_label   = local.public_ip_domain
+  domain_name_label   = "${var.name}-ingress"
 }
 
 resource "azurerm_kubernetes_cluster" "aks" {
@@ -22,7 +21,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   location            = data.azurerm_resource_group.resource_group.location
   resource_group_name = data.azurerm_resource_group.resource_group.name
   dns_prefix          = var.name
-  node_resource_group = local.nodes_resource_group
+  node_resource_group = azurerm_resource_group.nodes_resource_group.name
 
   default_node_pool {
     name       = "default"
