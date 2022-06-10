dfc679f59d0c252396a1426ee0d3b3e26fc814ad
Ahmed Zaman <ahmedrzaman@gmail.com>
2022-06-10 20:43:35 +0100

    Create a customer Azure Public IP

--- infrastructure/modules/resource_templates/aks/main.tf
+++ infrastructure/modules/resource_templates/aks/main.tf
@@ -1,13 +1,28 @@
+locals {
+  public_ip_name = "${var.name}-pip"
+  public_ip_domain = "${var.name}-ingress"
+  nodes_resource_group = "${var.name}-nodes-rg"
+}
+
 data "azurerm_resource_group" "resource_group" {
   name = var.resource_group_name
+}
+
+resource "azurerm_public_ip" "aks_pip" {
+  name                = local.public_ip_name
+  resource_group_name = local.nodes_resource_group
+  location            = data.azurerm_resource_group.resource_group.location
+  allocation_method   = "Static"
+  sku                 = "Standard"
+  domain_name_label   = local.public_ip_domain
 }
 
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = var.name
   location            = data.azurerm_resource_group.resource_group.location
   resource_group_name = data.azurerm_resource_group.resource_group.name
-  dns_prefix          = "${var.name}-dns"
-  node_resource_group = "${var.name}-nodes-rg"
+  dns_prefix          = var.name
+  node_resource_group = local.nodes_resource_group
 
   default_node_pool {
     name       = "default"
@@ -17,6 +32,13 @@ resource "azurerm_kubernetes_cluster" "aks" {
 
   identity {
     type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin = "kubenet"
+    load_balancer_profile {
+      outbound_ip_address_ids = [azurerm_public_ip.aks_pip.id]
+    }
   }
 }
 
--- infrastructure/modules/resource_templates/aks/outputs.tf
+++ infrastructure/modules/resource_templates/aks/outputs.tf
@@ -7,3 +7,10 @@ output "kube_config" {
   value     = azurerm_kubernetes_cluster.aks.kube_config_raw
   sensitive = true
 }
+
+output "aks_pip" {
+  value = {
+    name = azurerm_public_ip.aks_pip.name
+    resource_group_name = azurerm_public_ip.aks_pip.resource_group_name
+  }
+}
