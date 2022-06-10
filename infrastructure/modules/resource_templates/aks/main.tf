cc37fee3e8626d6366bf2b15b6bed1a33d0f398e
Ahmed Zaman <ahmedrzaman@gmail.com>
2022-06-10 17:52:18 +0100

    Add an applications nodepool

--- infrastructure/modules/resource_templates/aks/main.tf
+++ infrastructure/modules/resource_templates/aks/main.tf
@@ -6,7 +6,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   name                = var.name
   location            = data.azurerm_resource_group.resource_group.location
   resource_group_name = data.azurerm_resource_group.resource_group.name
-  dns_prefix          = "${var.name}-ingress"
+  dns_prefix          = "${var.name}-dns"
   node_resource_group = "${var.name}-nodes-rg"
 
   default_node_pool {
@@ -18,4 +18,11 @@ resource "azurerm_kubernetes_cluster" "aks" {
   identity {
     type = "SystemAssigned"
   }
+}
+
+resource "azurerm_kubernetes_cluster_node_pool" "application_pool" {
+  name = "applications"
+  node_count = 3
+  vm_size = "Standard_D2_v3"
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.aks.id
 }
