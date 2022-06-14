69bb7d47147ceba6fe0a3fe8a25220b6585ec6a4
Ahmed Zaman <ahmedrzaman@gmail.com>
2022-06-14 04:45:57 +0100

    No longer pass in provider into aks module

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -43,10 +43,6 @@ module "aks" {
   name                = local.aks_name
   resource_group_name = module.resource_group.resource_group_name
   depends_on          = [module.resource_group]
-
-  providers = {
-    kubectl = kubectl.aks
-  }
 }
 
 module "traffic_manager" {
