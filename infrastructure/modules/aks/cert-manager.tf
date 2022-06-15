499741640d7895bc1b27d7c3aaf8d2350a8cfa31
Ahmed Zaman <ahmedrzaman@gmail.com>
2022-06-15 22:17:45 +0100

    Deploy cert-manager via helm

---
+++ infrastructure/modules/aks/cert-manager.tf
@@ -0,0 +1,26 @@
+#
+# Deploy cert_manager to the AKS cluster
+#
+
+resource "kubernetes_namespace" "cert_manager" {
+  metadata {
+    name = "cert-manager"
+  }
+}
+
+resource "helm_release" "cert_manager" {
+  name       = "cert-manager"
+  repository = "https://charts.jetstack.io"
+  chart      = "cert-manager"
+  version    = "v1.7.1"
+  namespace  = kubernetes_namespace.cert_manager.metadata.0.name
+
+  set {
+    name  = "installCRDs"
+    value = "true"
+  }
+
+  depends_on = [
+    kubernetes_namespace.cert_manager
+  ]
+}
