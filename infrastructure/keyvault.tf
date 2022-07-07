ee76c2b5e946fd6237e25c4dc2f3e72998011774
Ahmed Zaman <ahmedrzaman@gmail.com>
2022-07-07 18:32:09 +0100

    Moving code to keyvualt.tf

--- infrastructure/keyvault.tf
+++ infrastructure/keyvault.tf
@@ -30,3 +30,15 @@ resource "azurerm_key_vault_access_policy" "deployer" {
   ]
 }
 
+resource "random_password" "centops_admin_key" {
+  length           = 30
+  lower            = true
+  upper            = true
+  override_special = "\\/-="
+}
+
+resource "azurerm_key_vault_secret" "centops_admin_key" {
+  name         = "CentOpsAdminApiKey"
+  value        = random_password.centops_admin_key.result
+  key_vault_id = azurerm_key_vault_access_policy.deployer.key_vault_id
+}
