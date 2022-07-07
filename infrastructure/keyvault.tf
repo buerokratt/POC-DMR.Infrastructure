b38d17184aa2dd46c07f920573c56d44f33a8a04
Ahmed Zaman <ahmedrzaman@gmail.com>
2022-07-07 18:41:36 +0100

    Broke out the centops stuff to its own file

--- infrastructure/keyvault.tf
+++ infrastructure/keyvault.tf
@@ -29,16 +29,3 @@ resource "azurerm_key_vault_access_policy" "deployer" {
     "Get", "List", "Update", "Create", "Import", "Delete", "Restore", "Purge"
   ]
 }
-
-resource "random_password" "centops_admin_key" {
-  length           = 30
-  lower            = true
-  upper            = true
-  override_special = "\\/-="
-}
-
-resource "azurerm_key_vault_secret" "centops_admin_key" {
-  name         = "CentOpsAdminApiKey"
-  value        = random_password.centops_admin_key.result
-  key_vault_id = azurerm_key_vault_access_policy.deployer.key_vault_id
-}
