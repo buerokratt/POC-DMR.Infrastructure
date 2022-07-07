c507cdb831fa9addb0e5da06eb3691d29ca6651a
Ahmed Zaman <ahmedrzaman@gmail.com>
2022-07-07 17:45:44 +0100

    Actually store the randomly generated password

--- infrastructure/centops.tf
+++ infrastructure/centops.tf
@@ -7,6 +7,6 @@ resource "random_password" "centops_admin_key" {
 
 resource "azurerm_key_vault_secret" "centops_admin_key" {
   name         = "CentOpsAdminApiKey"
-  value        = azurerm_cosmosdb_account.cosmos.primary_key
+  value        = random_password.centops_admin_key.result
   key_vault_id = azurerm_key_vault_access_policy.deployer.key_vault_id
 }
