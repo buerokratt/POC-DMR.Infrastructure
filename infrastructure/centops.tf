11d14d1e1401c7c3aaeb3fa89778c4fd0b12b5be
Ahmed Zaman <ahmedrzaman@gmail.com>
2022-07-07 14:53:09 +0100

    Generate and store centops admin key

---
+++ infrastructure/centops.tf
@@ -0,0 +1,12 @@
+resource "random_password" "centops_admin_key" {
+  length           = 14
+  lower            = true
+  upper            = true
+  override_special = "@:;\\/=?!$£"
+}
+
+resource "azurerm_key_vault_secret" "centops_admin_key" {
+  name         = "CentOps-AdminApiKey"
+  value        = azurerm_cosmosdb_account.cosmos.primary_key
+  key_vault_id = azurerm_key_vault_access_policy.deployer.key_vault_id
+}
