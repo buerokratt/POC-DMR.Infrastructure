dd1a9b1ec8854b581d47ef0d1c753c3e02434c7a
Ahmed Zaman <ahmedrzaman@gmail.com>
2022-07-08 14:40:44 +0100

    Installing cluster issuers

--- infrastructure/modules/aks/cert-manager.tf
+++ infrastructure/modules/aks/cert-manager.tf
@@ -27,3 +27,64 @@ resource "helm_release" "cert_manager" {
     kubernetes_namespace.cert_manager
   ]
 }
+
+resource "kubernetes_manifest" "cluster_issuer_staging" {
+  manifest = {
+    apiVersion = "cert-manager.io/v1"
+    kind       = "ClusterIssuer"
+    metadata = {
+      name = "letsencrypt-staging"
+    }
+
+    spec = {
+      acme = {
+        server = "https://acme-staging-v02.api.letsencrypt.org/directory"
+        email  = "buerokratt@ria.ee"
+        privateKeySecretRef = {
+          name = "letsencrypt-staging"
+        }
+        solvers = [{
+          http01 = {
+            ingress = {
+              class = "nginx"
+            }
+          }
+        }]
+      }
+    }
+  }
+  depends_on = [
+    helm_release.cert_manager
+  ]
+}
+
+
+resource "kubernetes_manifest" "cluster_issuer_prod" {
+  manifest = {
+    apiVersion = "cert-manager.io/v1"
+    kind       = "ClusterIssuer"
+    metadata = {
+      name = "letsencrypt"
+    }
+
+    spec = {
+      acme = {
+        server = "https://acme-v02.api.letsencrypt.org/directory"
+        email  = "buerokratt@ria.ee"
+        privateKeySecretRef = {
+          name = "letsencrypt"
+        }
+        solvers = [{
+          http01 = {
+            ingress = {
+              class = "nginx"
+            }
+          }
+        }]
+      }
+    }
+  }
+  depends_on = [
+    helm_release.cert_manager
+  ]
+}
