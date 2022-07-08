e56388ffff98244e5464921c6201eacfcf0b8f8f
Ahmed Zaman <ahmedrzaman@gmail.com>
2022-07-08 12:47:25 +0100

    Remove all helm addons

--- infrastructure/modules/aks/cert-manager.tf
+++ infrastructure/modules/aks/cert-manager.tf
@@ -1,29 +1,29 @@
-#
-# Deploy cert_manager to the AKS cluster
-#
+# #
+# # Deploy cert_manager to the AKS cluster
+# #
 
-resource "kubernetes_namespace" "cert_manager" {
-  metadata {
-    name = "cert-manager"
-  }
-  depends_on = [
-    azurerm_kubernetes_cluster_node_pool.application_pool
-  ]
-}
+# resource "kubernetes_namespace" "cert_manager" {
+#   metadata {
+#     name = "cert-manager"
+#   }
+#   depends_on = [
+#     azurerm_kubernetes_cluster_node_pool.application_pool
+#   ]
+# }
 
-resource "helm_release" "cert_manager" {
-  name       = "cert-manager"
-  repository = "https://charts.jetstack.io"
-  chart      = "cert-manager"
-  version    = "v1.8.2"
-  namespace  = kubernetes_namespace.cert_manager.metadata.0.name
+# resource "helm_release" "cert_manager" {
+#   name       = "cert-manager"
+#   repository = "https://charts.jetstack.io"
+#   chart      = "cert-manager"
+#   version    = "v1.8.2"
+#   namespace  = kubernetes_namespace.cert_manager.metadata.0.name
 
-  set {
-    name  = "installCRDs"
-    value = "true"
-  }
+#   set {
+#     name  = "installCRDs"
+#     value = "true"
+#   }
 
-  depends_on = [
-    kubernetes_namespace.cert_manager
-  ]
-}
+#   depends_on = [
+#     kubernetes_namespace.cert_manager
+#   ]
+# }
--- infrastructure/modules/aks/nginx.tf
+++ infrastructure/modules/aks/nginx.tf
@@ -15,44 +15,44 @@ resource "azurerm_public_ip" "nginx_ingress" {
   ]
 }
 
-resource "kubernetes_namespace" "nginx_ingress" {
-  metadata {
-    name = "nginx-ingress"
-  }
-  depends_on = [
-    azurerm_kubernetes_cluster.aks
-  ]
-}
+# resource "kubernetes_namespace" "nginx_ingress" {
+#   metadata {
+#     name = "nginx-ingress"
+#   }
+#   depends_on = [
+#     azurerm_kubernetes_cluster.aks
+#   ]
+# }
 
-resource "helm_release" "nginx" {
-  name             = "ingress-nginx"
-  repository       = "https://kubernetes.github.io/ingress-nginx"
-  chart            = "ingress-nginx"
-  namespace        = kubernetes_namespace.nginx_ingress.metadata.0.name
-  create_namespace = true
+# resource "helm_release" "nginx" {
+#   name             = "ingress-nginx"
+#   repository       = "https://kubernetes.github.io/ingress-nginx"
+#   chart            = "ingress-nginx"
+#   namespace        = kubernetes_namespace.nginx_ingress.metadata.0.name
+#   create_namespace = true
 
-  set {
-    name  = "controller.replicaCount"
-    value = 2
-  }
+#   set {
+#     name  = "controller.replicaCount"
+#     value = 2
+#   }
 
-  set {
-    name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/azure-load-balancer-health-probe-request-path"
-    value = local.health_endpoint
-  }
+#   set {
+#     name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/azure-load-balancer-health-probe-request-path"
+#     value = local.health_endpoint
+#   }
 
-  set {
-    name  = "controller.service.externalTrafficPolicy"
-    value = "Local"
-  }
+#   set {
+#     name  = "controller.service.externalTrafficPolicy"
+#     value = "Local"
+#   }
 
-  set {
-    name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/azure-dns-label-name"
-    value = azurerm_public_ip.nginx_ingress.domain_name_label
-  }
+#   set {
+#     name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/azure-dns-label-name"
+#     value = azurerm_public_ip.nginx_ingress.domain_name_label
+#   }
 
-  set {
-    name  = "controller.service.loadBalancerIP"
-    value = azurerm_public_ip.nginx_ingress.ip_address
-  }
-}
+#   set {
+#     name  = "controller.service.loadBalancerIP"
+#     value = azurerm_public_ip.nginx_ingress.ip_address
+#   }
+# }
