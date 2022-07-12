ab4abb0dfca77428ff9b7146bc9a4d8e6182380a
Ahmed Zaman <ahmedrzaman@gmail.com>
2022-07-12 16:18:05 +0100

    Generate API keys for DMR and MockClassifier

--- infrastructure/centops.tf
+++ infrastructure/centops.tf
@@ -1,12 +1,50 @@
+#
+# Generate CentOps Admin Key
+#
+
 resource "random_password" "centops_admin_key" {
   length           = 30
   lower            = true
   upper            = true
-  override_special = "\\/-="
+  override_special = "/-="
 }
 
 resource "azurerm_key_vault_secret" "centops_admin_key" {
   name         = "CentOpsAdminApiKey"
   value        = random_password.centops_admin_key.result
+  key_vault_id = azurerm_key_vault_access_policy.deployer.key_vault_id
+}
+
+#
+# Generate CentOps API Key for the DMR
+#
+
+resource "random_password" "dmr_api_key" {
+  length           = 30
+  lower            = true
+  upper            = true
+  override_special = "/-="
+}
+
+resource "azurerm_key_vault_secret" "dmr_api_key" {
+  name         = "DmrCentOpsApiKey"
+  value        = random_password.dmr_api_key.result
+  key_vault_id = azurerm_key_vault_access_policy.deployer.key_vault_id
+}
+
+#
+# Generate CentOps API Key for the Mock Classifier
+#
+
+resource "random_password" "mockclassifier_api_key" {
+  length           = 30
+  lower            = true
+  upper            = true
+  override_special = "/-="
+}
+
+resource "azurerm_key_vault_secret" "mockclassifer_api_key" {
+  name         = "MockClassifierCentOpsApiKey"
+  value        = random_password.mockbot_api_key.result
   key_vault_id = azurerm_key_vault_access_policy.deployer.key_vault_id
 }
