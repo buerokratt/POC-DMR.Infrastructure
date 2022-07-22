b2d36e6719da0b7ac1efeb1566b4d10e8afa35c0
Ahmed Zaman <ahmedrzaman@gmail.com>
2022-07-22 10:46:59 +0100

    Generate API key for 2 MockBots

--- infrastructure/centops.tf
+++ infrastructure/centops.tf
@@ -48,3 +48,35 @@ resource "azurerm_key_vault_secret" "mockclassifer_api_key" {
   value        = random_password.mockclassifier_api_key.result
   key_vault_id = azurerm_key_vault_access_policy.deployer.key_vault_id
 }
+
+#
+# Generate CentOps API Key for the Mock Bot 
+#
+
+# MockBot1
+resource "random_password" "mockbot1_api_key" {
+  length           = 30
+  lower            = true
+  upper            = true
+  override_special = "/-="
+}
+
+resource "azurerm_key_vault_secret" "mockbot1_api_key" {
+  name         = "MockBot1CentOpsApiKey"
+  value        = random_password.mockbot1_api_key.result
+  key_vault_id = azurerm_key_vault_access_policy.deployer.key_vault_id
+}
+
+# MockBot2
+resource "random_password" "mockbot2_api_key" {
+  length           = 30
+  lower            = true
+  upper            = true
+  override_special = "/-="
+}
+
+resource "azurerm_key_vault_secret" "mockbot2_api_key" {
+  name         = "MockBot2CentOpsApiKey"
+  value        = random_password.mockbot2_api_key.result
+  key_vault_id = azurerm_key_vault_access_policy.deployer.key_vault_id
+}
