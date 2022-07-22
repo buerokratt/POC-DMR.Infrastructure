58947062e76ad0b9a0efe2dc50d166608f52ac49
Guy Pritchard <gpritchard@neworbit.co.uk>
2022-07-22 14:33:56 +0100

    Fixing Deployed Environment Configuration (#69)
    Co-authored-by: Guy Pritchard <Guy.Pritchard@cdgad.onmicrosoft.com>
    Co-authored-by: Ahmed Zaman <ahmedrzaman@gmail.com>

--- infrastructure/centops.tf
+++ infrastructure/centops.tf
@@ -19,16 +19,16 @@ resource "azurerm_key_vault_secret" "centops_admin_key" {
 # Generate CentOps API Key for the DMR
 #
 
-resource "random_password" "dmr_api_key" {
+resource "random_password" "dmr_participant_key" {
   length           = 30
   lower            = true
   upper            = true
   override_special = "/-="
 }
 
-resource "azurerm_key_vault_secret" "dmr_api_key" {
+resource "azurerm_key_vault_secret" "dmr_participant_key" {
   name         = "DmrCentOpsApiKey"
-  value        = random_password.dmr_api_key.result
+  value        = random_password.dmr_participant_key.result
   key_vault_id = azurerm_key_vault_access_policy.deployer.key_vault_id
 }
 
@@ -36,15 +36,74 @@ resource "azurerm_key_vault_secret" "dmr_api_key" {
 # Generate CentOps API Key for the Mock Classifier
 #
 
-resource "random_password" "mockclassifier_api_key" {
+resource "random_password" "mockclassifier_participant_key" {
   length           = 30
   lower            = true
   upper            = true
   override_special = "/-="
 }
 
-resource "azurerm_key_vault_secret" "mockclassifer_api_key" {
+resource "azurerm_key_vault_secret" "mockclassifier_participant_key" {
   name         = "MockClassifierCentOpsApiKey"
-  value        = random_password.mockclassifier_api_key.result
+  value        = random_password.mockclassifier_participant_key.result
+  key_vault_id = azurerm_key_vault_access_policy.deployer.key_vault_id
+}
+
+#
+# Generate CentOps API Key for the Mock Bots
+#
+
+# MockBot1
+
+resource "random_password" "mockbot1_participant_key" {
+  length           = 30
+  lower            = true
+  upper            = true
+  override_special = "/-="
+}
+
+resource "azurerm_key_vault_secret" "mockbot1_participant_key" {
+  name         = "MockBot1CentOpsApiKey"
+  value        = random_password.mockbot1_participant_key.result
+  key_vault_id = azurerm_key_vault_access_policy.deployer.key_vault_id
+}
+
+resource "random_password" "mockbot1_chatapi_key" {
+  length           = 30
+  lower            = true
+  upper            = true
+  override_special = "/-="
+}
+
+resource "azurerm_key_vault_secret" "mockbot1_chatapi_key" {
+  name         = "MockBot1ApiKey"
+  value        = random_password.mockbot1_chatapi_key.result
+  key_vault_id = azurerm_key_vault_access_policy.deployer.key_vault_id
+}
+
+# MockBot2
+resource "random_password" "mockbot2_participant_key" {
+  length           = 30
+  lower            = true
+  upper            = true
+  override_special = "/-="
+}
+
+resource "azurerm_key_vault_secret" "mockbot2_participant_key" {
+  name         = "MockBot2CentOpsApiKey"
+  value        = random_password.mockbot2_participant_key.result
+  key_vault_id = azurerm_key_vault_access_policy.deployer.key_vault_id
+}
+
+resource "random_password" "mockbot2_chatapi_key" {
+  length           = 30
+  lower            = true
+  upper            = true
+  override_special = "/-="
+}
+
+resource "azurerm_key_vault_secret" "mockbot2_chatapi_key" {
+  name         = "MockBot2ApiKey"
+  value        = random_password.mockbot2_chatapi_key.result
   key_vault_id = azurerm_key_vault_access_policy.deployer.key_vault_id
 }
