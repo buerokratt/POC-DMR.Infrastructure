e055cfa02b2e36c3785540a3524e3cc72141b0df
Ahmed Zaman <ahmedrzaman@gmail.com>
2022-08-12 13:01:55 +0100

    Add demo storage

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -26,6 +26,14 @@ resource "azurerm_resource_group" "resource_group" {
   location = local.primary_location
 }
 
+resource "azurerm_storage_account" "demo" {
+  name                     = "demostore"
+  resource_group_name      = azurerm_resource_group.resource_group.name
+  location                 = azurerm_resource_group.resource_group.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+}
+
 module "aks" {
   source = "./modules/aks"
   name   = local.aks_name
