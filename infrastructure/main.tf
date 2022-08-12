9c1f1029e3991fe1b76493ca16fb26f1bb8114ba
Ahmed Zaman <ahmedrzaman@gmail.com>
2022-08-12 13:17:11 +0100

    Add byk to the demo storage account name

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -27,7 +27,7 @@ resource "azurerm_resource_group" "resource_group" {
 }
 
 resource "azurerm_storage_account" "demo" {
-  name                     = "byk-demostore"
+  name                     = "bykdemostore"
   resource_group_name      = azurerm_resource_group.resource_group.name
   location                 = azurerm_resource_group.resource_group.location
   account_tier             = "Standard"
